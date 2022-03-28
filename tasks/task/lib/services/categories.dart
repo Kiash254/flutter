44f0da9a22d186a8c4d88fb7861a41c293d0aa57
Kiash254 <codewitkiash@gmil.com>
2022-03-28 18:09:57 +0300

    second  commit

---
+++ tasks/task/lib/services/categories.dart
@@ -0,0 +1,13 @@
+class Category {
+  late String name;
+  late String Descriptiom;
+  late String store_number;
+  late String Ordered_by;
+  late String phone_number;
+  Category(
+      {required this.name,
+      required this.Descriptiom,
+      required this.Ordered_by,
+      required this.phone_number,
+      required this.store_number});
+}
